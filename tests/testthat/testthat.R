fdccc2dafa7c051a80e4961f5351c451ebed7875
simitpatel <simitp@gmail.com>
2016-04-11 05:11:57 -0500

    key
    Fixed key in testthat file

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -6,15 +6,15 @@ context("gpagespeed functions")
 test_check("gpagespeed")
 
 test_that("speedfinder function", {
-  expect_equal(class(speedfinder("https://www.cars.com","mobile","AIzaSyCj7QgHqxx81JtITkkZaLTfPM2GEvx8U1Q")),
+  expect_equal(class(speedfinder("https://www.cars.com","mobile",key)),
                class(data.frame()))
-  expect_equal(nrow(speedfinder("https://www.cars.com","mobile","AIzaSyCj7QgHqxx81JtITkkZaLTfPM2GEvx8U1Q")), 1)
+  expect_equal(nrow(speedfinder("https://www.cars.com","mobile",key)), 1)
   })
 
 test_that("speedlist function", {
   expect_equal(class(speedlist(c("https://www.cars.com","https://www.yahoo.com","https://www.techmeme.com"),
-                                 "mobile","AIzaSyCj7QgHqxx81JtITkkZaLTfPM2GEvx8U1Q")),
+                                 "mobile",key)),
                class(data.frame()))
   expect_equal(nrow(speedlist(c("https://www.cars.com","https://www.yahoo.com","https://www.techmeme.com"),
-                               "mobile","AIzaSyCj7QgHqxx81JtITkkZaLTfPM2GEvx8U1Q")), 3)
+                               "mobile",key)), 3)
 })
